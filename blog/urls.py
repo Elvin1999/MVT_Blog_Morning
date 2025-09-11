730d2925f05fd63e7c55205b6f47737626fc0064
Elvin1999 <camalzade_elvin@mail.ru>
2025-09-11 11:50:03 +0400

    Done for this blog project

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,8 @@ app_name = 'blog'
 urlpatterns = [
     path('',views.post_list,name='post_list'),
     path('post/<int:pid>',views.post_detail,name='post_detail'),
-    # path('post/new',views.post_create,name='post_create'),
+    path('post/new',views.post_create,name='post_create'),
     # path('post/<int:pid>/edit',views.post_edit,name='post_edit'),
-    # path('post/<int:pid>/delete',views.post_delete,name='post_delete'),
-    # path('search/',views.post_search,name='post_search'),
+    path('post/<int:pid>/delete',views.post_delete,name='post_delete'),
+    path('search/',views.post_search,name='post_search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,36 +1,79 @@
-from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseBadRequest
+from django.shortcuts import render, redirect
 from . import data
+from django.urls import reverse
+from django.contrib import messages
+
+from .data import Post
+
 # Create your views here.
 
 data.seed()
+
+
 def post_list(request):
-    posts=data.all_posts()
-    q=request.GET.get('q')
+    posts = data.all_posts()
+    q = request.GET.get('q')
     if q:
-        q_low=q.lower()
-        posts=[p for p in posts if q_low in p.title.lower() or q_low in p.content.lower()]
+        q_low = q.lower()
+        posts = [p for p in posts if q_low in p.title.lower() or q_low in p.content.lower()]
 
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
-def post_detail(request,pid:int):
-    post=data.get_post(pid)
+
+def post_detail(request, pid: int):
+    post = data.get_post(pid)
     if not post:
         return HttpResponseNotFound('Post did not find')
-    return render(request,'blog/post_detail.html',{'post':post})
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 
+def post_create(request):
+    print(request)
+    if request.method == 'GET':
+        return render(request, 'blog/post_form.html', {"mode": "create", "post": Post(0, "", "", "")})
+
+    # POST
+    title = (request.POST.get('title') or "").strip()
+    content = (request.POST.get('content') or "").strip()
+    author = (request.POST.get('author') or "").strip()
+
+    if not title or not content:
+        messages.error(request, 'Title and Content is required')
+        return render(request, 'blog/post_form.html',
+                      {"mode": "create", "title": title, "content": content, "author": author})
+
+    post = data.add_post(title, content, author)
+
+    messages.success(request, 'Post created successfully')
+    # return redirect('blog:post_detail',pid=post.id)
+    return redirect(reverse('blog:post_detail', args=[post.id]))
 
 
+def post_delete(request, pid: int):
+    post = data.get_post(pid)
+    if not post:
+        return HttpResponseNotFound('Post did not find')
+
+    if request.method == 'GET':
+        return render(request, "blog/post_confirm_delete.html", {"post": post})
+
+    ok = data.delete_post(pid)
+
+    if ok:
+        messages.success(request, 'Post deleted successfully')
+
+        return redirect(reverse('blog:post_list'))
+
+    return HttpResponseBadRequest("Error in delete operation")
 
 
+def post_search(request):
+    q = request.GET.get('q')
+    posts = data.all_posts()
 
+    if q:
+        q_low = q.lower()
+        posts = [p for p in posts if q_low in p.title.lower() or q_low in p.content.lower()]
 
-
-
-
-
-
-
-
-
+    return render(request, 'blog/post_list.html', {'posts': posts, "q": q})
